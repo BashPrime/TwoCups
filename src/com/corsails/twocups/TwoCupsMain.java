39869d18a8fbec677c74fe7c7dc015f266841101
Evan Taylor <etaylor8086@gmail.com>
2016-08-03 21:58:56 -0700

    Set up basic stuff for user input

--- src/com/corsails/twocups/TwoCupsMain.java
+++ src/com/corsails/twocups/TwoCupsMain.java
@@ -1,10 +1,33 @@
 package com.corsails.twocups;
 
+import java.util.Scanner;
+
 public class TwoCupsMain {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Set up CLI-style input
+		boolean exitFlag = false;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter three arguments: two cup sizes, and a target amount, all integers and separated by whitespace.");
+		System.out.print("> ");
+		
+		// Main scanner/user input loop
+		while (!exitFlag && scanner.hasNextLine()) {
+			String input = scanner.nextLine();
+			if (input.length() > 0) {
+				// Check for exit condition
+				if (input.equals("exit")) {
+					exitFlag = true;
+					System.out.println("Exiting...");
+				}
+				// Validate the input stream and begin processing it
+				else {
+					System.out.println("Do stuff here");
+				}
+			}
+			
+			if (!exitFlag)
+				System.out.print("> ");
+		}
+		
 	}
-
 }
