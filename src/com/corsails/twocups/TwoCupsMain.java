c05707763f7c0e22391d89ad2e7226bf9dc84a78
Evan Taylor <etaylor8086@gmail.com>
2016-08-03 22:55:34 -0700

    Core functionality has been implemented, yay
    -Added additional input validation; additional arguments inputted by the
    user are ignored.
    - Added measureWater() and isInteger() methods, the former being the
    most important part of the program

--- src/com/corsails/twocups/TwoCupsMain.java
+++ src/com/corsails/twocups/TwoCupsMain.java
@@ -3,7 +3,7 @@ package com.corsails.twocups;
 import java.util.Scanner;
 
 public class TwoCupsMain {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException {
 		// Set up CLI-style input
 		boolean exitFlag = false;
 		Scanner scanner = new Scanner(System.in);
@@ -19,9 +19,33 @@ public class TwoCupsMain {
 					exitFlag = true;
 					System.out.println("Exiting...");
 				}
+				
 				// Validate the input stream and begin processing it
 				else {
-					System.out.println("Do stuff here");
+					// Delimit by one or more whitespace characters, then check if there are at least 3 arguments; ignore additional ones
+					String[] inputArr = input.split(" +");
+					boolean validInputs = true;
+					
+					if (inputArr.length >= 3) {
+						// Validate that the arguments can be parsed into integers, then parse them
+						for (int i = 0; validInputs && i < 3; i++) {
+							String inputEntry = inputArr[i];
+							if (!isInteger(inputEntry)) {
+								System.out.println("An error occurred: Cannot parse string '" + inputEntry + "'");
+								validInputs = false;
+							}
+						}
+						
+						// If the invalid input flag doesn't trigger, the arguments are valid and we can try to measure the target water amount!
+						if (validInputs) {
+							int cup1 = Integer.parseInt(inputArr[0]);
+							int cup2 = Integer.parseInt(inputArr[1]);
+							int target = Integer.parseInt(inputArr[2]);
+							
+							boolean result = measureWater(cup1, cup2, target);
+							System.out.println(((result) ? "Yes!" : "No!") + " You " + ((result) ? "can" : "cannot") + " measure " + target + " ounces of water with a " + cup1 + "-ounce and a " + cup2 + "-ounce cup!");
+						}				
+					}
 				}
 			}
 			
@@ -29,5 +53,32 @@ public class TwoCupsMain {
 				System.out.print("> ");
 		}
 		
+	}
+	
+	public static boolean isInteger(String string) {
+	    try {
+	        Integer.valueOf(string);
+	        return true;
+	    } catch (NumberFormatException e) {
+	        return false;
+	    }
+	}
+	
+	public static boolean measureWater(int cup1, int cup2, int target) {
+		// If the cups are the same size, it's impossible to hit the target
+		if (cup1 == cup2)
+			return false;
+		
+		int smallerCup = (cup1 < cup2) ? cup1 : cup2;
+		int largerCup = (cup1 > cup2) ? cup1 : cup2;
+		int total = 0;
+		
+		// Get the difference between the larger cup and the smaller cup and add to the total measurement.
+		// Looking at this more abstractly, to hit the targeted measurement, you fill the larger cup until it's full, and then pour that cup into the smaller cup until you fill up the smaller cup.
+		// Whatever amount of water you have left over in the larger cup, you add to the total and attempt to hit the target measurement that way; you're guaranteed to either hit or exceed the target.
+		while (total < target) {
+			total += largerCup - smallerCup;
+		}
+		return (total == target);
 	}
 }
