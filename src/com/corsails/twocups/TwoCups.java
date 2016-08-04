6d3f7ebc2981eace80c0657b3a9a8ae0444fc290
Evan Taylor <etaylor8086@gmail.com>
2016-08-03 21:29:23 -0700

    Added source files to project

---
+++ src/com/corsails/twocups/TwoCups.java
@@ -0,0 +1,5 @@
+package com.corsails.twocups;
+
+public class TwoCups {
+
+}
---
+++ src/com/corsails/twocups/TwoCupsMain.java
@@ -0,0 +1,10 @@
+package com.corsails.twocups;
+
+public class TwoCupsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
